387d4aca045119305688d468c8bc6bf576f8f991
bx <binxin.zhou@outlook.com>
2025-04-13 14:15:22 +0800

    ✨ feat(dashboard): add HTML dashboard and integrate with worker

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
-import * as RainYun from './utils/rainyun'
-import * as NewAPI from './utils/newapi'
-import * as Serv00 from './utils/serv00'
+import * as RainYun from './utils/rainyun';
+import * as NewAPI from './utils/newapi';
+import * as Serv00 from './utils/serv00';
+
+// 导入HTML文件
+import indexHtml from '../public/index.html';
 
 export default {
-	async fetch (request: Request, env: Env, ctx: ExecutionContext) {
-		return new Response('This is a scheduled worker, no fetch handler defined.');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		// 使用外部HTML文件的内容
+		return new Response(indexHtml, {
+			headers: {
+				'Content-Type': 'text/html; charset=UTF-8',
+			},
+		});
 	},
-	async scheduled (
-		controller: ScheduledController,
-		env: Env,
-		ctx: ExecutionContext,
-	) {
+	async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext) {
 		switch (controller.cron) {
-			case "0 1 * * *":
+			case '0 1 * * *':
 				// 上海时间：9：00
 				break;
-			case "*/20 1-13 * * *":
+			case '*/20 1-13 * * *':
 				// 上海时间：9:00 - 21:00 每20分钟执行一次
 				await NewAPI.keep(env);
 				break;
-			case "0 0 * * 1":
+			case '0 0 * * 1':
 				// 每周一的0点执行一次
-				await Serv00.checkin(env)
+				await Serv00.checkin(env);
 				break;
 			default:
-				console.log("cron not match");
+				console.log('cron not match');
 		}
-		console.log("cron processed");
+		console.log('cron processed');
 	},
 };
